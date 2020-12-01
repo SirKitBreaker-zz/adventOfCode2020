21c1694381ad41d5c6d6a691418e08879d18c775
SirKitBreaker <sssahil4713@gmail.com>
2020-12-01 17:48:40 +0530

    Modified for Puzzle 2

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -3,20 +3,20 @@ from functools import reduce
 from operator import mul
 
 # Function to get Puzzle 1's answer
-def answer():
-    # Open file with input
+def answer(num):
+    # Open file with input numbers
     with open("input.txt", "r") as f:
         content = f.readlines()
-        # remove whitespace characters like `\n` at the end of each line
+        # remove whitespace characters like '\n' at the end of each line
         # and save it as a list of numbers
     content = [int(x.strip()) for x in content]
 
     # Iterate through list
-    for s in combinations(content, 2):
+    for s in combinations(content, num):
         # Find the numbers that add up to 2020
         if sum(s) == 2020:
             # Multiply them
             return reduce(mul, s)
 
 # Print the answer
-print(answer())
+# print(answer(3))
